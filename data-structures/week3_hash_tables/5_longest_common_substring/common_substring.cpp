ff6807dd07fc4becc6a72386570e1e888a710d32
avjotsingh <30839396+avjotsingh@users.noreply.github.com>
2020-07-31 18:26:40 +0530

    Update common_substring.cpp
    this completes the `hash tables` assignment

--- data-structures/week3_hash_tables/5_longest_common_substring/common_substring.cpp
+++ data-structures/week3_hash_tables/5_longest_common_substring/common_substring.cpp
@@ -1,143 +1,92 @@
 #include <iostream>
 #include <map>
 #include <chrono>
+#include <vector>
 using namespace std;
 using namespace std::chrono;
-
-// #define x1 31
-// #define x2 119
-// #define m1 1000000007
-// #define m2 1000000009
+using std::vector;
 
 struct Answer {
-	long long i, j, len;
+	size_t i, j, len;
 };
-
-// inline long long hash_1(const string &s) {
-// 	long long hash = 0;
-// 	for(long long i = 0; i < s.size(); ++i) {
-// 		hash = (hash * x1 + s[i]) % m1;
-// 	}
-// 	return hash;
-// }
-
-// inline long long update_hash1(long long h, long long m, char a, char b) {
-// 	h = h - (m * a) % m1;
-// 	if(h < 0)
-// 		h += m1;
-// 	h = (h * x1 + b) % m1;
-// 	return h;
-// }
-
-// inline long long hash_2(const string &s) {
-// 	long long hash = 0;
-// 	for(long long i = 0; i < s.size(); ++i) {
-// 		hash = (hash * x2 + s[i]) % m2;
-// 	}
-// 	return hash;
-// }
-
-// inline long long update_hash2(long long h, long long m, char a, char b) {
-// 	h = h - (m * a) % m2;
-// 	if(h < 0)
-// 		h += m2;
-// 	h = (h * x2 + b) % m2;
-// 	return h;
-// }
 
 Answer solve(const string &s, const string &t) {
 	Answer ans = {0, 0, 0};
-	long long low = 1;
-	long long high = s.size();
+	size_t low = 1;
+	size_t high = s.size();
 
-	long long x1, x2, m1, m2;
-	x1 = rand() % 1000000000;
-	x2 = rand() % 1000000000;
-	m1 = 10000000007;
-//	m2 = 1000000009;
+	size_t x1, x2, m1, m2;
+	x1 = 31;
+	x2 = 119;
+	m1 = 1000007;
+	m2 = 1000009;
 
 	while(low <= high) {
 		bool match = false;
-		long long k = (low + high) / 2;
+		size_t k = (low + high) / 2;
 		
-		std::map<long long, long long> hashes_1;
-//		std::map<long long, long long> hashes_2;	
-		long long h1, h2, mult1, mult2;
+		std::map<size_t, size_t> hashes_1;
+		std::map<size_t, size_t> hashes_2;	
+		size_t h1, h2, multiplier1, multiplier2;
 
-		mult1 = mult2 = 1;
-		for(long long i = 1; i <= k - 1; ++i) {
-			mult1 = (mult1 * x1) % m1;
-//			mult2 = (mult2 * x2) % m2;
+		multiplier1 = multiplier2 = 1;
+		for(size_t i = 1; i <= k - 1; ++i) {
+			multiplier1 = (multiplier1 * x1) % m1;
+			multiplier2 = (multiplier2 * x2) % m2;
 		}
 
-//		std::cout << "k: " << k <<'\n';
-		for(long long i = 0; i + k <= s.size(); ++i) {
-			// if(i == 0) {
-			// 	h1 = hash_1(s.substr(0, k));
-			// 	h2 = hash_2(s.substr(0, k));
-			// }
-			// else {
+		for(size_t i = 0; i + k <= s.size(); ++i) {
 			if(i == 0) {
 				h1 = h2 = 0;
-				for(long long i = 0; i < k; ++i) {
+				for(size_t i = 0; i < k; ++i) {
 					h1 = (h1 * x1 + s[i]) % m1;
-//					h2 = (h2 * x2 + s[i]) % m2;
+					h2 = (h2 * x2 + s[i]) % m2;
 				}
 			}
 			else {
-				h1 = h1 - (mult1 * s[i - 1]) % m1;
-				if(h1 < 0)
-					h1 += m1;
+				size_t a = (multiplier1 * s[i - 1]) % m1;
+				if(h1 >= a)
+					h1 = h1 - a;
+				else
+					h1 = (h1 + m1 - a) % m1;
 				h1 = (h1 * x1 + s[i + k - 1]) % m1;
 
-				// h2 = h2 - (mult2 * s[i - 1]) % m2;
-				// if(h2 < 0)
-				// 	h2 += m2;
-				// h2 = (h2 * x2 + s[i + k - 1]) % m2;
+				size_t b = (multiplier2 * s[i - 1]) % m2;
+				if(h2 >= b)
+					h2 = h2 - b;
+				else
+					h2 = (h2 + m2 - b) % m2;
+				h2 = (h2 * x2 + s[i + k - 1]) % m2;
 			}
-			//h1 = update_hash1(h1, mult1, s[i - 1], s[i + k - 1]);
-			//h2 = update_hash2(h2, mult2, s[i - 1], s[i + k - 1]);
-			//}
-
-//			std::cout << s << " i: " << i << " h1: " << h1 << " h2: " << h2 << '\n';
 			hashes_1.insert({h1, i});
-//			hashes_2.insert({h2, i});
+			hashes_2.insert({h2, i});
 		}
-
-		
-		for(long long i = 0; i + k <= t.size(); ++i) {
-			// if(i == 0) {
-			// 	h1 = hash_1(t.substr(0, k));
-			// 	h2 = hash_2(t.substr(0, k));
-			// }
-			// else {
-			//h1 = update_hash1(h1, mult1, t[i - 1], t[i + k - 1]);
-			//h2 = update_hash2(h2, mult2, t[i - 1], t[i + k - 1]);
-			//}
+		for(size_t i = 0; i + k <= t.size(); ++i) {
 			if(i == 0) {
 				h1 = h2 = 0;
-				for(long long i = 0; i < k; ++i) {
+				for(size_t i = 0; i < k; ++i) {
 					h1 = (h1 * x1 + t[i]) % m1;
-//					h2 = (h2 * x2 + t[i]) % m2;
+					h2 = (h2 * x2 + t[i]) % m2;
 				}
 			}
 			else {
-
-				h1 = h1 - (mult1 * t[i - 1]) % m1;
-				if(h1 < 0)
-					h1 += m1;
+				size_t a = (multiplier1 * t[i - 1]) % m1;
+				if(h1 >= a)
+					h1 = h1 - a;
+				else
+					h1 = (h1 + m1 - a) % m1;
 				h1 = (h1 * x1 + t[i + k - 1]) % m1;
 
-				// h2 = h2 - (mult2 * t[i - 1]) % m2;
-				// if(h2 < 0)
-				// 	h2 += m2;
-				// h2 = (h2 * x2 + t[i + k - 1]) % m2;
+				size_t b = (multiplier2 * t[i - 1]) % m2;
+				if(h2 >= b)
+					h2 = h2 - b;
+				else
+					h2 = (h2 + m2 - b) % m2;
+				h2 = (h2 * x2 + t[i + k - 1]) % m2;
 			}
-//			std::cout << t << " i: " << i << " h1: " << h1 << " h2: " << h2 << '\n';
-			if(hashes_1.find(h1) != hashes_1.end()){//} && hashes_2.find(h2) != hashes_2.end() && hashes_1[h1] == hashes_2[h2]) {
+			if(hashes_1.find(h1) != hashes_1.end() && hashes_2.find(h2) != hashes_2.end() && hashes_1[h1] == hashes_2[h2]) {
 				match = true;
 				ans = {hashes_1[h1], i, k};
-//				std::cout << "breaking...\n";
 				break;
 			}
 		}
@@ -153,11 +102,9 @@ Answer solve(const string &s, const string &t) {
 
 void test_solution() {
 	string s, t;
-	size_t l1 = 1 + rand() % 100000;
-	size_t l2 = 1 + rand() % 100000;
-	for(size_t i = 0; i < l1; ++i)
+	for(size_t i = 0; i < 100000; ++i)
 		s.append(string(1, 'a' + rand() % 25));
-	for(size_t i = 0; i < l2; ++i)
+	for(size_t i = 0; i < 100000; ++i)
 		t.append(string(1, 'a' + rand() % 25));
 	auto start = high_resolution_clock::now();
 	auto ans = solve(s, t);
